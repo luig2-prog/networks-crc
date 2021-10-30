5fb005b400607830a46adb7e0d8727167a8a5bad
luis hernandez <luisjimenezh8@gmail.com>
2021-10-30 14:38:16 -0500

    concatenado el numerador

--- js/crc.js
+++ js/crc.js
@@ -1,24 +1,31 @@
-const calcular =  () => {
-    let d;
-    let r;
-    let g;
-    let arrayDigitDR;
-    let arrayDigitG;
-    let subArray;
-    let result;
-    // private char[] outComeSubtraction;
-    // private char[] arrayDigitDR;
-    // private char[] arrayDigitG;
-    // private char[] subArray;
-    // private String result;
-    // private char[] outComeSubtraction;
-    
+const gg = '10011';
+const dd = '10011110001';
+const rr = 4;
+
+const calcularCrc = (g, d, r) => {
+
+    let numerador = armarNumerador(d, r);
+    console.log(numerador);
+
+    if(numerador === '100111100010000'){
+        console.log('Sii');
+    }
+
 }
 
+
+const armarNumerador = (primero, segundo) => { 
+    let concatenar = new Array();
+    for(let i = 0; i < segundo; i++) {
+        concatenar[i] =  '0';
+    }
+    return primero + concatenar.join('');
+}
+calcularCrc(gg, dd, rr);
 const btn_calcular = document.getElementById('btn_calcular');
-btn_calcular.addEventListener('click', calcular);
-
-
+btn_calcular.addEventListener('click', () => {
+    calcularCrc(gg, dd, rr);
+});
 
 
 
